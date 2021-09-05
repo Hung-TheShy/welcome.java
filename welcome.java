d21bb36f1cf6d151f0656fdbb333c15542b60400
Nguyễn Văn Hùng <89849671+Hung-TheShy@users.noreply.github.com>
2021-09-05 21:24:23 +0700

    Add files via upload

---
+++ welcome.java
@@ -0,0 +1,14 @@
+
+package java1;
+public class Java1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    System.out.println("welcome to java");
+  }
+    }
+    
+}
